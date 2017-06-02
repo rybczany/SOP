8cb48c4feceef6260d9a2c98e69f68021cb1be20
rybczany <piotr.maciej.rybak@gmail.com>
2017-06-02 15:11:03 +0200

    sync slider

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
-  $('.slider').slick({
+  $('.slider--autoplay').slick({
     slidesToShow: 1,
   	slidesToScroll: 1,
   	autoplay: true,
   	autoplaySpeed: 4000,
   });
+});
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  focusOnSelect: true
 });

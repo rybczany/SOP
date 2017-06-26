07591b6f6acf8e9ae1c0ebeb9dc00b5103d31302
rybczany <piotr.maciej.rybak@gmail.com>
2017-06-26 14:47:56 +0200

    RWD_customize4

--- js/sop.js
+++ js/sop.js
@@ -31,20 +31,22 @@ $(document).ready(function(){
   	autoplaySpeed: 3000,
   	dots: true,
   });
-});
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  focusOnSelect: true,
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  
+  $('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true,
+  });
 });
 
 
